85e4064a2eba477408f02f05b76fe38b3ee7af0c
root <root@vmi3508281.contaboserver.net>
2026-08-29 05:57:14 +0200

    Save work: automated commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ranchoraices.com',
+
   vite: {
     plugins: [tailwindcss()]
   },
